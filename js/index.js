fbc33401f0afef3d86d4abd7dbf6fcc6982bb18d
Ibrahim <ibrahimimaduddin82@gmail.com>
2021-06-22 01:32:34 +0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,5 @@
+// hamburger toggle
+const hamburger = document.querySelector('.hamburger');
+hamburger.addEventListener('click', function () {
+  document.querySelector('.mb-navbar').classList.toggle('active');
+});
